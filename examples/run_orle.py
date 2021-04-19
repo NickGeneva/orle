708ebfd81293155cdbdd1cdb895b4691b33ef4fa
Nicholas Geneva <ngeneva@nd.edu>
2021-04-19 16:04:12 -0400

    Added env run script to examples folder

--- examples/run_orle.py
+++ examples/run_orle.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
 
-    sys.argv = sys.argv + ["--config_path", "./orle_universe.yml"]
+    sys.argv = sys.argv + ["--config_path", "./orle_universe_viscosity.yml"]
     sys.argv = sys.argv + ["--world_id", "0"]
     args = Parser().parse()  
 
